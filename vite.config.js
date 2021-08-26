71d41151506b7370d986bf3b4166b6072b50a4fd
Warren Parad <wparad@gmail.com>
2021-08-26 17:44:21 +0200

    Improve vite config.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+// https://vitejs.dev/guide/build.html#public-base-path
+
 export default defineConfig({
   plugins: [vue()],
   base: './'
