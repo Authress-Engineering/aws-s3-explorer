1e655cc13dcbb85a22089d76225749105ec69051
Warren Parad <wparad@users.noreply.github.com>
2022-01-20 16:14:35 +0100

    Set total completion percentage to 0 if error.

--- src/components/uploadModal.vue
+++ src/components/uploadModal.vue
@@ -149,7 +149,7 @@ const uploadFiles = () => {
       DEBUG.log('File:', file, 'Part:', evt.part, evt.loaded, 'of', evt.total);
       if (typeof evt.loaded == "number" && typeof evt.total == "number"){
         // Use evt.loaded and evt.total to calculate the percentage of data uploaded
-        state.completionPercentageMap[fileIndex] = Math.round((evt.loaded * 100) / evt.total);
+        state.completionPercentageMap[fileIndex] = Math.round(evt.loaded * 100 / evt.total) || 0;
       }
     };
 
